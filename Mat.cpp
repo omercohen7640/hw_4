89ee15a513ec410b92a7b11bc63c1d4552c0f7bb
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2019-12-28 12:00:41 +0200

    matrix class is ready

--- Mat.cpp
+++ Mat.cpp
@@ -3,3 +3,105 @@
 //
 
 #include "Mat.h"
+
+/*********************************************************************************
+Function name: Mat
+Description: constructor with no parameters
+Parameters: None
+Return value: None
+**********************************************************************************/
+Mat::Mat()
+{
+    matrix_ = new int*[2];
+    for (int i=0; i<2; i++)
+    {
+        matrix_[i] = new int[2];
+        for (int j=0; j<2; j++)
+        {
+            matrix_[i][j] = 0;
+        }
+    }
+}
+
+/*********************************************************************************
+Function name: Mat
+Description: constructor
+Parameters: rows - height dimension
+            cols - weight dimension
+Return value: None
+**********************************************************************************/
+Mat::Mat(int rows, int cols) : Rows_(rows), Cols_(cols)
+{
+    matrix_ = new int*[Rows_];
+    for (int i=0; i<Rows_; i++)
+    {
+        matrix_[i] = new int[Cols_];
+        for (int j=0; j<Cols_; j++)
+        {
+            matrix_[i][j] = 0;
+        }
+    }
+}
+
+/*********************************************************************************
+Function name: ~Mat
+Description: destructor
+Parameters: None
+Return value: None
+**********************************************************************************/
+Mat::~Mat()
+{
+    for(int i=0; i<Rows_; i++)
+    {
+        delete []matrix_;
+    }
+}
+
+//----------------------------------------getters------------------------------------------//
+/*********************************************************************************
+Function name: getRowsNum
+Description: gets the number of rows
+Parameters: None
+Return value: int - the height dimension
+**********************************************************************************/
+int Mat::getRowsNum()
+{
+    return Rows_;
+}
+
+/*********************************************************************************
+Function name: getColumnsNum
+Description: gets the number of columns
+Parameters: None
+Return value: int - the weight dimension
+**********************************************************************************/
+int Mat::getColumnsNum()
+{
+    return Cols_;
+}
+
+/*********************************************************************************
+Function name: getElement
+Description: gets value of the element in the indices given
+Parameters: row - index of row
+            col - index of column
+Return value: int - the value of element
+**********************************************************************************/
+int Mat::getElement(int row, int col)
+{
+    return matrix_[row-1][col-1];
+}
+
+//----------------------------------------setters------------------------------------------//
+/*********************************************************************************
+Function name: setElement
+Description: changes the value of an element in the indices given
+Parameters: row - index of row
+            col - index of column
+            val - the value to put in the matrix
+Return value: None
+**********************************************************************************/
+void Mat::setElement(int row, int col, int val)
+{
+    matrix_[row-1][col-1] = val;
+}
--- Mat.h
+++ Mat.h
@@ -8,6 +8,67 @@
 
 class Mat {
 
+    public:
+/*********************************************************************************
+Function name: Mat
+Description: constructor with no parameters
+Parameters: None
+Return value: None
+**********************************************************************************/
+        Mat ();
+/*********************************************************************************
+Function name: Mat
+Description: constructor
+Parameters: rows - height dimension
+            cols - weight dimension
+Return value: None
+**********************************************************************************/
+        Mat(int rows, int cols);
+/*********************************************************************************
+Function name: ~Mat
+Description: destructor
+Parameters: None
+Return value: None
+**********************************************************************************/
+        virtual ~Mat();
+        //getters:
+/*********************************************************************************
+Function name: getRowsNum
+Description: gets the number of rows
+Parameters: None
+Return value: int - the height dimension
+**********************************************************************************/
+        int getRowsNum();
+/*********************************************************************************
+Function name: getColumnsNum
+Description: gets the number of columns
+Parameters: None
+Return value: int - the weight dimension
+**********************************************************************************/
+        int getColumnsNum();
+/*********************************************************************************
+Function name: getElement
+Description: gets value of the element in the indices given
+Parameters: row - index of row
+            col - index of column
+Return value: int - the value of element
+**********************************************************************************/
+        int getElement(int row, int col);
+        //setters:
+/*********************************************************************************
+Function name: setElement
+Description: changes the value of an element in the indices given
+Parameters: row - index of row
+            col - index of column
+            val - the value to put in the matrix
+Return value: None
+**********************************************************************************/
+        void setElement(int row, int col, int val);
+    private:
+        int Rows_;
+        int Cols_;
+        int** matrix_;
+
 };
 
 
